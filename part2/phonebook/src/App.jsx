464fc2699fd284a5bb2b39c342272c09fc214fec
Mykyta <bidok24@gmail.com>
2023-12-12 12:09:14 -0500

    phonebook app update following ex 2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
 
+const DisplayPerson = ({persons}) => {
+  return persons.map(person => <li key={person.name}>{person.name}</li>)
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas'}
@@ -8,36 +12,41 @@ const App = () => {
 
   const addNumber = (event) => {
     event.preventDefault()
-    const numbetObj = {
+    const numberObj = {
       name: newName
     }
 
-    setPersons(persons.concat(numbetObj))
+    if (persons.filter(person => person.name === newName).length) {
+      console.log(persons.filter(person => person.name === newName))
+      alert(`${newName} is already in phonebook`)
+      return setNewName(newName)
+    }
+
+    setPersons(persons.concat(numberObj))
     setNewName('')
   }
-
-  return (
-
-<div>
-  <h2>Phonebook</h2>
-  <form onSubmit={addNumber}>
-    <div>
-      name <input
-        value={newName}
-        onChange={(event)=>{setNewName(event.target.value)}}
-      />
-    </div>
-    <div>
-      <button type = "submit">add</button>  
-    </div>  
-  </form>  
-  <h2>Numbers</h2>
-  <ul>
-    {persons.map(person => <li key={person.name}>{person.name}</li>)}
-  </ul>
-</div>
-
-  )
-}
+return (
+  <div>
+    <h2>Phonebook</h2>
+    <form onSubmit={addNumber}>
+      <div>
+        name <input
+          value={newName}
+          onChange={(event)=>{setNewName(event.target.value)}}
+        />
+      </div>
+      <div>
+        number <input />
+      </div>
+      <div>
+        <button type = "submit">add</button>  
+      </div>  
+    </form>  
+    <h2>Numbers</h2>
+    <ul>
+      <DisplayPerson persons={persons} />
+    </ul>
+  </div>
+)}
 
 export default App
